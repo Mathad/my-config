836bf7b4f9d277006de3f72514bc73c339b91d75
Mathieu Hadjimegrian <mathieu.hadjimegrian@non.se.com>
2019-03-22 15:45:39 +0100

    emacs : reorganizing core and modules

--- emacs/core/core.el
+++ emacs/core/core.el
@@ -2,6 +2,26 @@
 ;;;;          MATHAD's EMACS CONFIG          ;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;     LIBS -- MODULES     ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Dash - a modern list API for Emacs
+(add-to-list 'load-path "~/my-config/emacs/modules/libs/dash/dash.el")
+
+;; Popup
+(load-file "~/my-config/emacs/modules/libs/popup/popup.el")
+
+;; Memoize
+(load-file "~/my-config/emacs/modules/libs/emacs-memoize/memoize.el")
+
+;; S - string manipulation
+(load-file "~/my-config/emacs/modules/libs/s/s.el")
+(require 's)
+
+;; With Editor - This library makes it possible to reliably use the Emacsclient as the '$EDITOR' of child processes
+(add-to-list 'load-path "~/my-config/emacs/modules/libs/with-editor/")
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;     UI -- MODULES     ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -31,4 +51,4 @@
       telephone-line-evil-use-short-tag t)
 (telephone-line-mode 1)
 
-(provide 'ui-modules)
+(provide 'core)
