9978580a291a9cffaec5db8e8678bd8e4e8f16bf
Mathieu Hadjimegrian <mathieu.hadjimegrian@non.se.com>
2019-04-02 15:59:27 +0200

    emacs : deleting cask useless files

--- emacs/modules/.emacs.d/.cask/25.2/elpa/deadgrep-20190314.2207/deadgrep-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; deadgrep-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "deadgrep" "deadgrep.el" (23715 23122 443625
-;;;;;;  605000))
-;;; Generated autoloads from deadgrep.el
-
-(autoload 'deadgrep "deadgrep" "\
-Start a ripgrep search for SEARCH-TERM.
-If called with a prefix argument, create the results buffer but
-don't actually start the search.
-
-\(fn SEARCH-TERM)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; deadgrep-autoloads.el ends here
