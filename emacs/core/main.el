71f95264ef7a2bd716bda04321a0c16fb739575e
Mathieu Hadjimegrian <mathieu.hadjimegrian@non.se.com>
2019-04-12 14:43:52 +0200

    emacs : cleaning main file
    - cleaning duplicates

--- emacs/core/main.el
+++ emacs/core/main.el
@@ -33,56 +33,26 @@
 
 ;; Use 'a' binding in dired mode 
 (put 'dired-find-alternate-file 'disabled nil)
-
 ;; ;; Loading my Emacs environment
 (add-to-list 'load-path "~/my-config/emacs/modules/")
-
 ;; Loading my theme : spacegray
 (load-file "~/my-config/emacs/core/themes/spacegray.el")
-
 ;; Set default web browser
 (setq browse-url-browser-function 'browse-url-firefox
       browse-url-new-window-flag  t
 browse-url-firefox-new-window-is-tab t)
 ;; Allow to save password
 (setq password-cache-expiry nil)
-
-;; Set some general variables
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(column-number-mode t)
- '(menu-bar-mode nil)
- '(tool-bar-mode nil)
- '(tramp-copy-size-limit 10240000))
-
 ;; Hide welcome screen
 (setq inhibit-startup-message t)
-
 ;; Show parenthese
 (show-paren-mode 1)
-
-;; Dired Extra
-(add-hook 'dired-load-hook
-            (function (lambda () (load "dired-x"))))
-
 ;; Remove scroll bar
 (scroll-bar-mode -1)
-
 ;; Set default shell
 (setq explicit-shell-file-name "/bin/bash")
-
 ;; Change default grep command
 (setq grep-command "grep --color -nsrH -e ")
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-)
 
 ;; Loading my  modules
 (require 'modules)
