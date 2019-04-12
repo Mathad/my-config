01ed976c3d2b67a82965df7ca1c675f0455693e1
Mathieu Hadjimegrian <mathieu.hadjimegrian@non.se.com>
2019-04-12 15:17:06 +0200

    emacs : typo main files

--- emacs/core/main.el
+++ emacs/core/main.el
@@ -2,11 +2,6 @@
 ;;;;          MATHAD's EMACS CONFIG          ;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -24,11 +19,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+)
 
-;; Loading my Emacs environment
+;; loading my Emacs environment
 (add-to-list 'load-path "~/my-config/emacs/modules/")
-;; Loading my theme : spacegray
+;; loading my theme : spacegray
 (load-file "~/my-config/emacs/core/themes/spacegray.el")
 
 ;; set Firefox as default browser
@@ -41,23 +36,23 @@ browse-url-firefox-new-window-is-tab t)
 (setq password-cache t)
 (setq password-cache-expiry 3600)
 
-;; Use 'a' binding in dired mode 
+;; use 'a' binding in dired mode 
 (put 'dired-find-alternate-file 'disabled nil)
-;; Hide welcome screen
+;; hide welcome screen
 (setq inhibit-startup-message t)
-;; Show parenthese
+;; show parenthese
 (show-paren-mode 1)
-;; Remove scroll bar
+;; remove scroll bar
 (scroll-bar-mode -1)
-;; Set default shell
+;; set default shell
 (setq explicit-shell-file-name "/bin/bash")
-;; Change default grep command
+;; change default grep command
 (setq grep-command "grep --color -nsrH -e ")
 
-;; Loading my  modules
+;; my modules configuration
 (require 'modules)
 
-;; Loading my keybindings
+;; my keybindings
 (require 'keybindings)
 
 (provide 'main)
